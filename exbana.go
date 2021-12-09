6b5b7110de66eb1a049c94137fe05d16d6b1e0b2
almerlucke <almer.lucke@gmail.com>
2021-12-09 14:37:16 +0100

    added concatenation and identifier

--- exbana.go
+++ exbana.go
@@ -14,6 +14,66 @@ type PatternMatchResult struct {
 }
 
 type PatternMatcher interface {
-	Match(EntityStreamer) (bool, PatternMatchResult)
+	Match(EntityStreamer) (bool, *PatternMatchResult)
 	Identifier() string
+}
+
+type SingleEntityMatchFunction func(interface{}) bool
+
+type SingleEntityMatch struct {
+	identifier    string
+	matchFunction SingleEntityMatchFunction
+}
+
+func NewSingleEntityMatch(identifier string, matchFunction SingleEntityMatchFunction) *SingleEntityMatch {
+	return &SingleEntityMatch{
+		identifier:    identifier,
+		matchFunction: matchFunction,
+	}
+}
+
+func (m *SingleEntityMatch) Identifier() string {
+	return m.identifier
+}
+
+func (m *SingleEntityMatch) Match(s EntityStreamer) (bool, *PatternMatchResult) {
+	pos := s.Position()
+
+	if m.matchFunction(s.Read()) {
+		return true, &PatternMatchResult{
+			StartPosition: pos,
+			EndPosition:   s.Position(),
+		}
+	}
+
+	return false, nil
+}
+
+type ConcatenationMatch struct {
+	identifier string
+	patterns   []PatternMatcher
+}
+
+func NewConcatenationMatch(identifier string, patterns []PatternMatcher) *ConcatenationMatch {
+	return &ConcatenationMatch{
+		identifier: identifier,
+		patterns:   patterns,
+	}
+}
+
+func (m *ConcatenationMatch) Identifier() string {
+	return m.identifier
+}
+
+func (m *ConcatenationMatch) Match(s EntityStreamer) (bool, *PatternMatchResult) {
+	pos := s.Position()
+
+	for _, pm := range m.patterns {
+		match, _ := pm.Match(s)
+		if !match {
+			return false, nil
+		}
+	}
+
+	return true, &PatternMatchResult{StartPosition: pos, EndPosition: s.Position()}
 }
--- exbana_test.go
+++ exbana_test.go
@@ -4,6 +4,78 @@ import (
 	"testing"
 )
 
+type TestStream struct {
+	values []int
+	pos    int
+}
+
+func NewTestStream() *TestStream {
+	return &TestStream{
+		values: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		pos:    0,
+	}
+}
+
+func (ts *TestStream) Peek() interface{} {
+	if ts.pos < len(ts.values) {
+		return ts.values[ts.pos]
+	}
+
+	return nil
+}
+
+func (ts *TestStream) Read() interface{} {
+	if ts.pos < len(ts.values) {
+		v := ts.values[ts.pos]
+		ts.pos += 1
+		return v
+	}
+
+	return nil
+}
+
+func (ts *TestStream) Finished() bool {
+	return ts.pos >= len(ts.values)
+}
+
+func (ts *TestStream) Position() int {
+	return ts.pos
+}
+
+func (ts *TestStream) SetPosition(pos int) {
+	ts.pos = pos
+}
+
 func TestExbana(t *testing.T) {
+	s := NewTestStream()
+	sem := NewSingleEntityMatch("match even ints", func(entity interface{}) bool { return entity.(int)%2 == 0 })
+	cm := NewConcatenationMatch("match 5, 6", []PatternMatcher{
+		NewSingleEntityMatch("5", func(entity interface{}) bool { return entity.(int) == 5 }),
+		NewSingleEntityMatch("6", func(entity interface{}) bool { return entity.(int) == 6 }),
+	})
+	var m PatternMatcher = sem
+
+	t.Logf("%v", m.Identifier())
+
+	for !s.Finished() {
+		match, result := m.Match(s)
+		if match {
+			t.Logf("match %v - %v", result.StartPosition, result.EndPosition)
+		}
+	}
+
+	m = cm
+
+	s.SetPosition(0)
+
+	t.Logf("%v", m.Identifier())
+
+	for !s.Finished() {
+		match, result := m.Match(s)
+		if match {
+			t.Logf("match %v - %v", result.StartPosition, result.EndPosition)
+		}
+	}
+
 	t.Log("check")
 }
